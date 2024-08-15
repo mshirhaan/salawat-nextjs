87c66e50f581c83ba495d371af6aa0920b41a401
Mohammed Shirhaan <mshirhaan@gmail.com>
2024-08-15 13:11:56 +0530

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 // app/layout.tsx
-import Head from "next/head";
 import { Providers } from "./providers";
 
 import "./globals.css";
@@ -16,18 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Amiri:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <body>
         <Providers>{children}</Providers>
       </body>
