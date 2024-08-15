8fb413d88411957d6c8278afc35a1f5c4840250d
Mohammed Shirhaan <mshirhaan@gmail.com>
2024-08-15 09:16:53 +0530

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Amiri:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Providers>{children}</Providers>
       </body>
