da5d0e4b737110feb0727f6e7dee4cd978f3dc55
Mohammed Shirhaan <mshirhaan@gmail.com>
2024-08-16 17:57:01 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-'use client';
+"use client";
 
-import { Box, Flex, VStack, Heading, Text, Button, Image, Container, useColorModeValue, keyframes, Icon } from "@chakra-ui/react";
-import Link from 'next/link';
+import {
+  Box,
+  Flex,
+  VStack,
+  Heading,
+  Text,
+  Button,
+  Image,
+  Container,
+  useColorModeValue,
+  keyframes,
+  Icon,
+} from "@chakra-ui/react";
+import Link from "next/link";
 import { motion } from "framer-motion";
 import { FaHeart, FaMosque, FaStar } from "react-icons/fa";
 
@@ -29,11 +41,22 @@ export default function Home() {
       </Box>
 
       <Container maxW="container.xl">
-        <Flex direction={{ base: "column", md: "row" }} align="center" justify="center" minH="100vh" py={10}>
-          <VStack spacing={8} align="flex-start" flex={1} pr={{ base: 0, md: 8 }}>
-            <Heading 
-              as={motion.h1} 
-              size="2xl" 
+        <Flex
+          direction={{ base: "column", md: "row" }}
+          align="center"
+          justify="center"
+          minH="100vh"
+          py={10}
+        >
+          <VStack
+            spacing={8}
+            align="flex-start"
+            flex={1}
+            pr={{ base: 0, md: 8 }}
+          >
+            <Heading
+              as={motion.h1}
+              size="2xl"
               color={textColor}
               initial={{ opacity: 0, y: -20 }}
               animate={{ opacity: 1, y: 0 }}
@@ -41,26 +64,29 @@ export default function Home() {
             >
               Welcome to the Salawat App
             </Heading>
-            <Text 
-              fontSize="xl" 
+            <Text
+              fontSize="xl"
               color={textColor}
               as={motion.p}
               initial={{ opacity: 0, y: -10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.2 }}
             >
-              Embrace the love for Prophet Muhammad ﷺ through the blessed practice of reciting Salawat.
+              Embrace the love for Prophet Muhammad ﷺ through the blessed
+              practice of reciting Salawat.
             </Text>
-            <Text 
-              fontSize="lg" 
-              color={accentColor} 
+            <Text
+              fontSize="lg"
+              color={accentColor}
               fontStyle="italic"
               as={motion.p}
               initial={{ opacity: 0, y: -10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.4 }}
             >
-              "Indeed, Allah and His angels send blessings upon the Prophet. O you who have believed, ask [Allah to confer] blessing upon him and ask [Allah to grant him] peace." [Quran 33:56]
+              &rdquo;Indeed, Allah and His angels send blessings upon the
+              Prophet. O you who have believed, ask [Allah to confer] blessing
+              upon him and ask [Allah to grant him] peace.&rdquo; [Quran 33:56]
             </Text>
             <Button
               as={Link}
@@ -92,13 +118,28 @@ export default function Home() {
       </Container>
 
       {/* Floating hearts */}
-      <Box position="absolute" top="20%" left="10%" animation={`${float} 3s ease-in-out infinite`}>
+      <Box
+        position="absolute"
+        top="20%"
+        left="10%"
+        animation={`${float} 3s ease-in-out infinite`}
+      >
         <Icon as={FaHeart} w={6} h={6} color="red.400" />
       </Box>
-      <Box position="absolute" top="60%" right="15%" animation={`${float} 4s ease-in-out infinite`}>
+      <Box
+        position="absolute"
+        top="60%"
+        right="15%"
+        animation={`${float} 4s ease-in-out infinite`}
+      >
         <Icon as={FaHeart} w={4} h={4} color="red.300" />
       </Box>
-      <Box position="absolute" bottom="15%" left="20%" animation={`${float} 5s ease-in-out infinite`}>
+      <Box
+        position="absolute"
+        bottom="15%"
+        left="20%"
+        animation={`${float} 5s ease-in-out infinite`}
+      >
         <Icon as={FaHeart} w={5} h={5} color="red.500" />
       </Box>
     </Box>
